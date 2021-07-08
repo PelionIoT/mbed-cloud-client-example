14836172a1bc7b66fc6579186ffe4c9bd4f3ddcc
Chun-Chieh Li <ccli8@nuvoton.com>
2021-07-08 09:37:31 +0800

    Fix compile error for Mbed TF-M V8M target
    Use DEVICE_FLASH to exclude KVStore/FLASHIAP code

--- source/platform/mbed-os/mcc_common_setup.cpp
+++ source/platform/mbed-os/mcc_common_setup.cpp
@@ -386,11 +386,14 @@ void mcc_platform_sw_build_info(void) {
 int mcc_platform_storage_init(void) {
     // This wait will allow the board more time to initialize
     mcc_platform_do_wait(MCC_PLATFORM_WAIT_BEFORE_BD_INIT * SECONDS_TO_MS);
+    // Normally, TF-M V8M targets don't support KVStore/FLASHIAP and change to PSA Storage API instead.
+#if DEVICE_FLASH
     int status = kv_init_storage_config();
     if (status != MBED_SUCCESS) {
         printf("kv_init_storage_config() - failed, status %d\n", status);
         return status;
     }
+#endif
     return 0;
 }
 
